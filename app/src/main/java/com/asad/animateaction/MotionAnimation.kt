96e440463e9b4281c3f4ead9b472e2924b896db3
ASADMUKHTAR <asadmukhtar28@gmail.com>
2021-11-30 03:33:40 +0500

    Remove Unnecessary files,  Improve the animation.

--- app/src/main/java/com/asad/animateaction/MotionAnimation.kt
+++ app/src/main/java/com/asad/animateaction/MotionAnimation.kt
@@ -7,6 +7,7 @@ import com.asad.animateaction.databinding.ActivityMotionAnimationBinding
 
 class MotionAnimation : AppCompatActivity() {
     var transitionName = ""
+    var isReverseAnimationRequired = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,39 +18,45 @@ class MotionAnimation : AppCompatActivity() {
         )
 
         binding.view.setOnClickListener {
-            when (transitionName) {
-                getString(R.string.interest_anim) ->
-                    binding.motionAnim.setTransition(
-                        R.id.interests_trans_end,
-                        R.id.interests_trans_start
-                    )
-                getString(R.string.profile_anim) ->
-                    binding.motionAnim.setTransition(
-                        R.id.profile_trans_end,
-                        R.id.profile_trans_start
-                    )
-                getString(R.string.preference_anim) ->
-                    binding.motionAnim.setTransition(
-                        R.id.preferences_trans_end,
-                        R.id.preferences_trans_start
-                    )
-                getString(R.string.notification_anim) ->
-                    binding.motionAnim.setTransition(
-                        R.id.notification_trans_end,
-                        R.id.notification_trans_start
-                    )
-            }
+            if (isReverseAnimationRequired) {
+                when (transitionName) {
+                    getString(R.string.interest_anim) ->
+                        binding.motionAnim.setTransition(
+                            R.id.interests_trans_end,
+                            R.id.interests_trans_start
+                        )
+                    getString(R.string.profile_anim) ->
+                        binding.motionAnim.setTransition(
+                            R.id.profile_trans_end,
+                            R.id.profile_trans_start
+                        )
+                    getString(R.string.preference_anim) ->
+                        binding.motionAnim.setTransition(
+                            R.id.preferences_trans_end,
+                            R.id.preferences_trans_start
+                        )
+                    getString(R.string.notification_anim) ->
+                        binding.motionAnim.setTransition(
+                            R.id.notification_trans_end,
+                            R.id.notification_trans_start
+                        )
+                }
 
-            binding.motionAnim.transitionToEnd()
+                binding.motionAnim.transitionToEnd()
+                isReverseAnimationRequired = false
+            }
         }
 
         binding.tvInterests.setOnClickListener {
+            isReverseAnimationRequired = true
             transitionName = getString(R.string.interest_anim)
             binding.motionAnim.transitionName = transitionName
             binding.motionAnim.setTransition(R.id.interests_trans_start, R.id.interests_trans_end)
             binding.motionAnim.transitionToEnd()
         }
+
         binding.tvPreferences.setOnClickListener {
+            isReverseAnimationRequired = true
             transitionName = getString(R.string.preference_anim)
             binding.motionAnim.transitionName = transitionName
             binding.motionAnim.setTransition(
@@ -59,6 +66,7 @@ class MotionAnimation : AppCompatActivity() {
             binding.motionAnim.transitionToEnd()
         }
         binding.tvNotifications.setOnClickListener {
+            isReverseAnimationRequired = true
             transitionName = getString(R.string.notification_anim)
             binding.motionAnim.transitionName = transitionName
             binding.motionAnim.setTransition(
@@ -68,6 +76,7 @@ class MotionAnimation : AppCompatActivity() {
             binding.motionAnim.transitionToEnd()
         }
         binding.tvProfile.setOnClickListener {
+            isReverseAnimationRequired = true
             transitionName = getString(R.string.profile_anim)
             binding.motionAnim.transitionName = transitionName
             binding.motionAnim.setTransition(
